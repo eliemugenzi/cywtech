f35dfbcae3d195bd686e29ccfa4d5a06261d0887
AiMuhire <AIM@myPositivo.kuikdelivery.com>
2018-02-20 18:08:49 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  
+
+  $(document).ready(function(){
+    var options ={scrollOffset:100};
+    $('.scrollspy').scrollSpy(options);
+  });
